76329d5cdc1d944a80c470fd50990a4d0f9d1260
Sriram-Mullapudi <srirammullapudi20@gmail.com>
2025-07-27 06:11:19 -0400

    Initial commit with Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("I like pizza");
+        System.out.println("it is good");
+        System.out.println( );
+        System.out.println( );
+    }
+}
