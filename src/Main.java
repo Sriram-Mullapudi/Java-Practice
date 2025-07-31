e336e22eec4a9575a52e9827b6d3176adbd9a156
Sriram-Mullapudi <srirammullapudi20@gmail.com>
2025-07-31 02:43:47 -0400

    Initial commit with Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("I like pizza");
-        System.out.println("it is good");
-        System.out.println( );
-        System.out.println( );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your email: ");
+
+        String email;
+        String username;
+        String domain;
+
+        email = scanner.nextLine();
+
+        if (email.contains("@")) {
+            username = email.substring(0, email.indexOf("@"));
+            domain = email.substring(email.indexOf("@") + 1);
+            System.out.println(username);
+            System.out.println(domain);
+        }
+        else{
+            System.out.println("Email must contain '@'");
+        }
+
+
+
+
+        scanner.close();
     }
 }
