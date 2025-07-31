a9a73a384bc16b43cb70621822dc462d32158a7a
Sriram-Mullapudi <srirammullapudi20@gmail.com>
2025-07-31 03:43:00 -0400

    Initial commit with WeightConversion.java

---
+++ src/TrenaryOperator.java
@@ -0,0 +1,13 @@
+public class TrenaryOperator {
+    public static void main(String[] args){
+
+        // trenary operator ? = Return 1 or 2 values if a conditon is true
+
+        int income = 60000;
+
+        double taxRate = (income>=40000) ? 0.25 : 0.15;
+
+        System.out.println(taxRate);
+
+    }
+}
